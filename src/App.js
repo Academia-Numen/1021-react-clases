fb07e58b4060bbf89de20841f078be89db07cec5
Aaron Ivan Amaro <aaronivanamaro@gmail.com>
2022-04-05 20:45:36 -0300

    Ejemplo Lista con Keys y Estado (Clase 4)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import { useState } from "react";
-import Form from "./components/Form";
-import Header from "./components/Header";
-import Listas from "./components/Listas";
-import Main from "./components/Main";
-import PropagationExample from "./components/PropagationExample";
+
+function ListItem ({ id, nombre, eliminarItem }) {
+
+  return <li onClick={() => eliminarItem(id)}>{nombre}</li>
+}
 
 function App() {
+  const [items, setItems] = useState([
+    { id: 1, value: 'Regar las plantas' },
+    { id: 2, value: 'Salir a correr' },
+    { id: 3, value: 'Tomar agua' },
+    { id: 4, value: 'Estudiar React' },
+  ]);
 
-  const [title, setTitle] = useState('Título');
+  function eliminarLista (identificador) {
+    const listaFiltrada = items.filter(item => item.id !== identificador);
+    setItems(listaFiltrada)
+  }
 
   return (
     <div>
-      <h1>{title}</h1>
-      <Main>
-        <Header />
-        <Form />
-        <PropagationExample />
-        <Listas />
-      </Main>
+      <ul>
+        {
+          items.map((item, index) => {
+            return(
+              <ListItem key={item.id} nombre={item.value} eliminarItem={eliminarLista} id={item.id}/>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
