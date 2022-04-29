66ef187acf0c1cbb775ff7670f3a48d2b1b66dec
Aaron Ivan Amaro <aaronivanamaro@gmail.com>
2022-04-29 20:23:04 -0300

    Clase 7: Ejemplo reducer con contador

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import BookList from "./components/BookList";
-import Navbar from "./components/Navbar";
-import ToggleButton from "./components/ToggleButton";
-import BooksContextProvider from "./contexts/BooksContext";
-import ThemeContextProvider from "./contexts/ThemeContext";
+import Counter from "./components/Counter";
+import CounterWithReducer from "./components/CounterWithReducer";
 
 function App() {
 
   return (
     <div className="App">
-
-      <ThemeContextProvider>
-
-        <Navbar />
-
-        <BooksContextProvider>
-          <div>
-            <div>
-              <BookList />
-            </div>
-          </div>
-        </BooksContextProvider>
-
-        <ToggleButton />
-
-      </ThemeContextProvider>
-
+      {/* <Counter /> */}
+      <CounterWithReducer />
     </div>
   )
 }
